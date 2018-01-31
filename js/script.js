adde7cf77854e807affa19ba3f4d7e4b282a84e6
Brian Canedo <bcanedo4@gmail.com>
2018-01-31 13:13:46 -0800

    Cleaned up font sizes for different window sizes

--- js/script.js
+++ js/script.js
@@ -139,24 +139,22 @@ $( document ).ready(function() {
 
 	// When the user clicks the button, open the modal 
 	btn.onclick = function() {
-			modal.style.display = "block";
-			// console.log("kd");
+		modal.style.display = "block";
 	}
 
 	btnSmall.onclick = function() {
 		modal.style.display = "block";
-		// console.log("kd");
 	}
 
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
-			modal.style.display = "none";
+		modal.style.display = "none";
 	}
 
 	// When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
-			if (event.target == modal) {
-					modal.style.display = "none";
-			}
+		if (event.target == modal) {
+				modal.style.display = "none";
+		}
 	}
 });
